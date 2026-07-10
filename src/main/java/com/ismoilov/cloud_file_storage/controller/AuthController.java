3090b02fb02a57967318932807af8d948a331600
munir <ismoilov.munir97@gmail.com>
2026-07-10 19:43:42 +0300

    feat: add Swagger documentation to FileController and AuthController

--- src/main/java/com/ismoilov/cloud_file_storage/controller/AuthController.java
+++ src/main/java/com/ismoilov/cloud_file_storage/controller/AuthController.java
@@ -3,19 +3,21 @@ package com.ismoilov.cloud_file_storage.controller;
 import com.ismoilov.cloud_file_storage.dto.RegisterRequest;
 import com.ismoilov.cloud_file_storage.entity.User;
 import com.ismoilov.cloud_file_storage.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Tag(name = "Auth", description = "Auth management API")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
-
+    @Operation(summary = "create User ", description = "create User to storage")
     @PostMapping("register")
     public ResponseEntity<User> createUser (@RequestBody RegisterRequest registerRequest) {
 
--- src/main/java/com/ismoilov/cloud_file_storage/controller/FileController.java
+++ src/main/java/com/ismoilov/cloud_file_storage/controller/FileController.java
@@ -2,6 +2,8 @@ package com.ismoilov.cloud_file_storage.controller;
 
 import com.ismoilov.cloud_file_storage.service.MinioService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -13,30 +15,36 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.InputStream;
 import java.util.List;
 
+@Tag(name = "Files", description = "File management API")
 @RestController
 @RequestMapping("/api/files")
 @RequiredArgsConstructor
 public class FileController {
     private final MinioService minioService;
 
+
+    @Operation(summary = "Upload file", description = "Upload file to storage")
     @PostMapping("/upload")
     public ResponseEntity<Void> upload(@RequestParam("file") MultipartFile file, @RequestParam("path") String fileName) {
         minioService.uploadFile(file, fileName);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "listFiles file", description = "listFiles file to storage")
     @GetMapping()
     public ResponseEntity<List<String>> listFiles(@RequestParam(value = "path", required = false, defaultValue = "") String prefix) {
         return ResponseEntity.ok().body(minioService.listFiles(prefix));
 
     }
 
+    @Operation(summary = "deleteFile file", description = "deleteFile file to storage")
     @DeleteMapping()
     public ResponseEntity<Void> deleteFile(@RequestParam(value = "path") String fileName) {
         minioService.deleteFile(fileName);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "download file", description = "download file to storage")
     @GetMapping("/download")
     public ResponseEntity<Resource> download(@RequestParam(value = "path") String fileName) {
         InputStream inputStream = minioService.downloadFile(fileName);
@@ -45,6 +53,7 @@ public class FileController {
                 .body(new InputStreamResource(inputStream));
     }
 
+    @Operation(summary = "createFolder file", description = "createFolder file to storage")
     @PostMapping("/folder")
     public ResponseEntity<Void> createFolder(@RequestParam(value = "path") String folderName) {
         minioService.createFolder(folderName);
@@ -52,12 +61,14 @@ public class FileController {
     }
 
 
+    @Operation(summary = "move file", description = "move file to storage")
     @PostMapping("/move")
     public ResponseEntity<Void> move(@RequestParam(value = "source") String source, @RequestParam(value = "target") String target) {
         minioService.moveFile(source, target);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "search file", description = "search file to storage")
     @GetMapping("/search")
     public ResponseEntity<List<String>> search(@RequestParam(value = "query") String query) {
         return ResponseEntity.ok().body(minioService.searchFile(query));
